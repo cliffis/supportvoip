7fff0c0d0538ffdae1af4ef9fe76cc1e3586a8bf
UC_cliffis <cliffis@yandex.ru>
2022-02-09 18:27:08 +0100

    Update ami_settings.py

--- ami_settings.py
+++ ami_settings.py
@@ -1,9 +1,9 @@
 connection = {
-    'address': '10.45.2.10',
+    'address': 'ip_address',
     'port': 5038
 }
 
 login = {
     'username': 'adminconf',
-    'secret': '58a3d20cc970b350f4c8ef5294c988dc'
+    'secret': 'password'
 }
